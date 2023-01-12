9d34c1221a7f7e546ac8c7bed57fdb4d10c1089e
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2023-01-12 18:24:27 +0300

    Completed Assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [ isAdded, setAdded ] = useState(false);
+  const itemClass = isAdded ? "in-cart" : "";
+
+  function handleCart(){
+    setAdded(!isAdded);
+  }
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{isAdded ? "Remove from cart": "Add to Cart"}</button>
     </li>
   );
 }
